9cbe6684547febff94f35b19db6344895639e4bd
Nick Jocius <capacity512@yahoo.com>
2021-12-24 17:43:33 -0600

    added responsive styles for tab and desktop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,13 @@
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
+const content = document.querySelector(".content");
 
 /* Toggle hamburger menu */
 
 function toggleMenu() {
     if (menu.classList.contains("active")) {
         menu.classList.remove("active");
+       
     } else {
         menu.classList.add("active");
     }
