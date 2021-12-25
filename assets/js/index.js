0a0d161b18a4e807a3d795752abad5e0f9d3e66f
Nick Jocius <capacity512@yahoo.com>
2021-12-24 19:41:36 -0600

    finished js functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
-const content = document.querySelector(".content");
+const form = document.getElementById("myForm");
+const message = document.querySelector(".alert");
 
 /* Toggle hamburger menu */
 
@@ -13,5 +14,26 @@ function toggleMenu() {
     }
 }
 
+/*Form submit function */
+function handleSubmit(e) {
+    e.preventDefault();
+    
+    let name = form.name.value;
+    let email = form.email.value;
+    let password = form.password.value;
+
+    if (name != "" && email != "" && password != "") {
+        let msg = document.createElement('p');
+        msg.textContent = "Success";
+        message.appendChild(msg);
+        message.classList.add("success")
+        setTimeout(() => {
+            message.classList.remove("success");
+        }, 2000);
+    }
+    
+}
+
 /* Event Listener */
 toggle.addEventListener("click", toggleMenu, false);
+form.addEventListener("submit", handleSubmit);
